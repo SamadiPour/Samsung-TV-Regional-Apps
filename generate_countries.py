6b65cd01e1c249cb8f6d866b7f38da79c946c186
SamadiPour <samadipoor2@gmail.com>
2023-05-08 16:53:08 +0330

    style: convert " to '

--- generate_countries.py
+++ generate_countries.py
@@ -7,16 +7,16 @@ countries = sorted([(country['cca2'], country['name']['common']) for country in 
 valid_countries = []
 for country in countries:
     r = requests.get(
-        url="https://vdapi.samsung.com/tvs/tvpersonalize/api/tvapps/appserver/list",
+        url='https://vdapi.samsung.com/tvs/tvpersonalize/api/tvapps/appserver/list',
         params={
-            "country_code": country[0],
-            "language_code": "en-US",
-            "offset": "0",
-            "size": "1",
+            'country_code': country[0],
+            'language_code': 'en-US',
+            'offset': '0',
+            'size': '1',
         }
     )
     data = r.json()
-    if data.get("data", {}).get("allCount", 0) > 0:
+    if data.get('data', {}).get('allCount', 0) > 0:
         valid_countries.append(country)
 
 with open('output/countries.txt', 'w') as file:
