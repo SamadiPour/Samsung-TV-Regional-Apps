42069aeb5b9a69c96544699c4b68a8ceeebc218b
SamadiPour <samadipoor2@gmail.com>
2023-05-08 16:36:08 +0330

    feat: add code for generating and validating countries

---
+++ generate_countries.py
@@ -0,0 +1,23 @@
+import requests
+
+r = requests.get(url='https://restcountries.com/v3.1/all')
+data = r.json()
+countries = sorted([(country['cca2'], country['name']['common']) for country in data], key=lambda x: x[0])
+
+valid_countries = []
+for country in countries:
+    r = requests.get(
+        url="https://vdapi.samsung.com/tvs/tvpersonalize/api/tvapps/appserver/list",
+        params={
+            "country_code": country[0],
+            "language_code": "en-US",
+            "offset": "0",
+            "size": "1",
+        }
+    )
+    data = r.json()
+    if data.get("data", {}).get("allCount", 0) > 0:
+        valid_countries.append(country)
+
+with open('output/countries.txt', 'w') as file:
+    file.write('\n'.join([f'{country[0]}, {country[1]}' for country in valid_countries]))
